b9a8ffe4bab72ca8cee92a8566d21a2ed7d3fd0c
nmorse <morse.nate@gmail.com>
2020-11-29 20:08:33 -0500

    flag waving

--- src/main.js
+++ src/main.js
@@ -8,14 +8,14 @@ const a_PounceProgramEle = document.getElementById("user-pl-a");
 // const exampleSelectEle = document.getElementById("example");
 
 const initProgram = decodeURI(location.hash.substr(1))?.split(';');
-let r_pounceProgram = initProgram[0] ? initProgram[0] : 'x 2 % 2 /';
-let g_pounceProgram = initProgram[1] ? initProgram[1] : 'y 3 % 3 /';
-let b_pounceProgram = initProgram[2] ? initProgram[2] : 'x 64 /';
-let a_pounceProgram = initProgram[3] ? initProgram[3] : '0.2';
+let r_pounceProgram = initProgram[0] ? initProgram[0] : 'x 64 / dup * y 64 / dup * + sqrt ';
+let g_pounceProgram = initProgram[1] ? initProgram[1] : 'y 6 % 4 /';
+let b_pounceProgram = initProgram[2] ? initProgram[2] : '48 x dup * y dup * + sqrt - 24 / dup  *';
+let a_pounceProgram = initProgram[3] ? initProgram[3] : '40 y -';
 let logLevel = 0;
 
 // red
-r_PounceProgramEle.addEventListener("keyup", (e) => {
+r_PounceProgramEle.addEventListener("blur", (e) => {
     if (e.target.value !== r_pounceProgram) {
         r_pounceProgram = e.target.value;
         repl(`${r_pounceProgram} ${g_pounceProgram} ${b_pounceProgram} ${a_pounceProgram}`, logLevel);
@@ -30,7 +30,7 @@ r_PounceProgramEle.value = r_pounceProgram;
 r_PounceProgramEle.focus();
 
 // green
-g_PounceProgramEle.addEventListener("keyup", (e) => {
+g_PounceProgramEle.addEventListener("blur", (e) => {
     if (e.target.value !== g_pounceProgram) {
         g_pounceProgram = e.target.value;
         repl(`${r_pounceProgram} ${g_pounceProgram} ${b_pounceProgram} ${a_pounceProgram}`, logLevel);
@@ -43,7 +43,7 @@ g_PounceProgramEle.addEventListener("keyup", (e) => {
 g_PounceProgramEle.value = g_pounceProgram;
 
 //blue
-b_PounceProgramEle.addEventListener("keyup", (e) => {
+b_PounceProgramEle.addEventListener("blur", (e) => {
     if (e.target.value !== b_pounceProgram) {
         b_pounceProgram = e.target.value;
         repl(`${r_pounceProgram} ${g_pounceProgram} ${b_pounceProgram} ${a_pounceProgram}`, logLevel);
@@ -56,7 +56,7 @@ b_PounceProgramEle.addEventListener("keyup", (e) => {
 b_PounceProgramEle.value = b_pounceProgram;
 
 // alpha
-a_PounceProgramEle.addEventListener("keyup", (e) => {
+a_PounceProgramEle.addEventListener("blur", (e) => {
     if (e.target.value !== a_pounceProgram) {
         a_pounceProgram = e.target.value;
         repl(`${r_pounceProgram} ${g_pounceProgram} ${b_pounceProgram} ${a_pounceProgram}`, logLevel);
